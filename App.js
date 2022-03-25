542ea8794eac02ee0141604eff34008cdbb4e788
unknown <you@example.com>
2022-03-25 16:37:19 -0300

    inicial

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './src/components/Cabecalho';
+
+export default function App() {
+  return (
+    <View>
+<Cabecalho/>
+    </View>
+  );
+}
+
