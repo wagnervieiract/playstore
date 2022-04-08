e56a4a0aec37a842dffc66ad3b749254993b32fb
unknown <you@example.com>
2022-04-08 16:40:34 -0300

    projeto flatlist incompleto

---
+++ src/dados/dadosJogos.js
@@ -0,0 +1,33 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome : "Subway Surfers",
+        valor : 13.32,
+        imagem: 'subway.jpg'
+    },
+    {
+        id:"02",
+        nome:"Yugioh Master Duel ",
+        valor:"gratuito",
+        imagem:"yugi.jpg"
+    },
+    {
+        id:"03",
+        nome:'Pokemon go',
+        valor:'gratuito',
+        imagen:'pokemon.jpg'
+    },
+    {
+        id:"04",
+        nome:"Hora de Aventura",
+        valor:'10.00',
+        imagen:'aventura.jpg'
+    },
+    {
+        id:"05",
+        nome:"Dragon Ball Fight",
+        valor:'18.00',
+        imagen:'dbz.jpg'
+    }
+]
+export default JOGOS;
